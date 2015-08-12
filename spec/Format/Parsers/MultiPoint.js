bf8f06ca6e050170a2c064bb5f7f956b9ca9b8c9
kuzkok <kuzkok@gmail.com>
2015-08-12 11:53:30 +0500

    multipoint test fix

--- spec/Format/Parsers/MultiPoint.js
+++ spec/Format/Parsers/MultiPoint.js
@@ -15,11 +15,11 @@ describe("L.GML.MultiPoint", function () {
 
   it('should know how parse gml:Point', function () {
     var child = parser.parsers['gml:Point'];
-    expect(child).to.be.instanceOf(L.GML.Point);
+    expect(child).to.be.instanceOf(L.GML.PointNode);
   });
 
   it('should return L.FeatureGroup object', function () {
-    var stub = sinon.stub(L.GML.MultiGeometry.prototype, 'parse').returns([new L.Marker([0, 0])]);
+    var stub = sinon.stub(L.GML.MultiGeometry.prototype, 'parse').returns([[0, 0]]);
     var result = parser.parse({});
     expect(result).to.be.instanceOf(L.FeatureGroup);
     stub.restore();
